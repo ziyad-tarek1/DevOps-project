22f44a956de400e7540066d305a119cd7d085073
ziyad-tarek1 <ziyadtarek180@gmail.com>
2024-07-08 02:33:51 +0300

    Docker staged done

--- db.go
+++ db.go
@@ -13,35 +13,30 @@ var _connection *sql.DB
 func init() {
 	mysqlConfig := &mysqldriver.Config{
 		User:                 os.Getenv("MYSQL_USER"),
-		Passwd:               os.Getenv("MYSQL_PASS"),
-		Net:                  "tcp",
-		Addr:                 fmt.Sprintf("%s:%s", os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT")),
-		AllowNativePasswords: true,
-		ParseTime:            true,
+			Passwd:               os.Getenv("MYSQL_PASS"),
+			Net:                  "tcp",
+			Addr:                 fmt.Sprintf("%s:%s", os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT")),
+			AllowNativePasswords: true,
+			ParseTime:            true,
 	}
 
-	connection, err := sql.Open("mysql", mysqlConfig.FormatDSN())
-	if err != nil {
+	var err error
+	_connection, err = sql.Open("mysql", mysqlConfig.FormatDSN())
+	if err!= nil {
 		panic(err)
 	}
 
-	_, err = connection.Exec(`CREATE DATABASE IF NOT EXISTS internship`)
-	if err != nil {
+	_, err = _connection.Exec(`CREATE DATABASE IF NOT EXISTS internship`)
+	if err!= nil {
 		panic(err)
 	}
 
 	mysqlConfig.DBName = `internship`
 
-	if err = connection.Close(); err != nil {
+	_, err = _connection.Exec(`USE internship`)
+	if err!= nil {
 		panic(err)
 	}
-
-	connection, err = sql.Open("mysql", mysqlConfig.FormatDSN())
-	if err != nil {
-		panic(err)
-	}
-
-	_connection = connection
 
 	schema()
 }
@@ -52,7 +47,7 @@ func schema() {
 		created_at DATETIME NOT NULL
 	)`)
 
-	if err != nil {
+	if err!= nil {
 		panic(err)
 	}
 }
